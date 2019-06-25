333e88a97ab6f73ed6b1cf1899452978a40be100
Tomas <tomasfreire@gmail.com>
2019-06-25 18:32:56 -0500

    build

---
+++ public/precache-manifest.b38d52653daae5103bd6e6e9ba80ed68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea2d07baa13d7f78447c77acedccd8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67654dda7430850ce1f5",
+    "url": "/static/css/2.858b5ee9.chunk.css"
+  },
+  {
+    "revision": "2e3aba0863dd16a7fa4f",
+    "url": "/static/css/main.a1bd6fbb.chunk.css"
+  },
+  {
+    "revision": "67654dda7430850ce1f5",
+    "url": "/static/js/2.fac19595.chunk.js"
+  },
+  {
+    "revision": "2e3aba0863dd16a7fa4f",
+    "url": "/static/js/main.c2882808.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8be93cc020d23795c159e246dc5ec27c",
+    "url": "/static/media/logo_transparent copy.8be93cc0.png"
+  },
+  {
+    "revision": "4933acf9f1a7e2bab4d7665f33994e3f",
+    "url": "/static/media/lukas-blazek-261682-unsplash.4933acf9.jpg"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3889c808e862fd783431f05eb2d7d1d.js"
+  "/precache-manifest.b38d52653daae5103bd6e6e9ba80ed68.js"
 );
 
 self.addEventListener('message', (event) => {
